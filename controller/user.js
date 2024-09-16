7a87c085ffa6656152dffe601f7224371aa923ac
HEMANTAHUJA <ahujahemant449@gmaill.com>
2024-09-16 10:59:03 +0530

    main

--- controller/user.js
+++ controller/user.js
@@ -1,17 +1,36 @@
 const users = require("../user");
+const User = require("../model/user");
+const user = require("../model/user");
 
 
-exports.getUsers = (req,res)=>{
+exports.getUsers = async (req,res)=>{
 
     // res.send({message : "User fetched",data : users});
-    res.status(200).send({message : "user fetched",data : users}); // this can be used to send ststus messgae and error message to to server
+    // res.status(200).send({message : "user fetched",data : users}); // this can be used to send ststus messgae and error message to to server
+    
+    try{
+    const users = await User.find();
+    res.status(200).send({message: "User Fetched",data : users});
+    }catch(error){
+        res.status(500).send(error);
+    }
 };
 
-exports.crateUser = (req,res)=>{
-    const data  = req.body;
-    const modifiedData = {id : users.length+1 , ...data};
-    users.push(modifiedData);
-    res.status(201).send({message : "user created", data : data});
+exports.crateUser = async (req,res)=>{
+    try{
+    const {firstName , lastName, email, contactDetail}  = req.body;
+    // const modifiedData = {id : users.length+1 , ...data};
+    // users.push(modifiedData);
+    // res.status(201).send({message : "user created", data : data});
+    const newUser = new User({firstName : firstName , lastName : lastName, email : email, contactDetails : contactDetail});
+    await newUser.save();
+    res.status(201).send({message: "User created",data: user});
+    } catch(error){
+        console.log(error)
+        res.status(500).send({error : error});
+    }
+
+
 };
 
 exports.updateUser = (req,res)=>{
